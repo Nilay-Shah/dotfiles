74fb16a12209634f7466b956e2f70d6e880cbd16
Nilay Shah <Nilay-Shah@users.noreply.github.com>
2017-04-05 21:39:37 -0400

    Include Bash-it

--- .bash_it/template/bash_profile.template.bash
+++ .bash_it/template/bash_profile.template.bash
@@ -1,59 +1,7 @@
-# Git stuff
-function stashdiff() {
-	git stash show -p stash@{$1};
-}
-
-source ~/git-completion.bash
-
-# Don't record some commands
-export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
-
-# Correct spelling errors in arguments supplied to cd
-shopt -s cdspell;
-
-# Networking. IP address, dig, DNS
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-
-# show hidden files starting with a dot, and sort
-alias ll='ls -lGaf'
-
-function man() {
-    LESS_TERMCAP_mb=$'\e'"[1;31m" \
-    LESS_TERMCAP_md=$'\e'"[1;31m" \
-    LESS_TERMCAP_me=$'\e'"[0m" \
-    LESS_TERMCAP_se=$'\e'"[0m" \
-    LESS_TERMCAP_so=$'\e'"[1;44;33m" \
-    LESS_TERMCAP_ue=$'\e'"[0m" \
-    LESS_TERMCAP_us=$'\e'"[1;32m" \
-    command man "$@"
-}
-
-
-# Go stuff
-export GOPATH="/Users/Nilay/Dropbox/Gocode"
-PATH="$PATH:$GOPATH/bin"
-
-
-# Python stuff
-function venv() {
-	python3 -m venv $HOME/.venvs/$1;
-	chmod +x $HOME/.venvs/$1/bin/activate;
-}
-function activate() {
-	source $HOME/.venvs/$1/bin/activate;
-}
-
-
-PATH=$PATH:~/usr/local/bin
-
-export PATH
-
-
-
-#--------------- BASH-IT ---------------#
+#!/usr/bin/env bash
 
 # Path to the bash it configuration
-export BASH_IT="/Users/nilay/.bash_it"
+export BASH_IT="{{BASH_IT}}"
 
 # Lock and Load a custom theme file
 # location /.bash_it/themes/
