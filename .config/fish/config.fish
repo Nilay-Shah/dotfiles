cfba6dd8186ae023a6b3acf1bdc9b7ffa2e01660
Nilay Shah <Nilay-Shah@users.noreply.github.com>
2019-03-19 09:46:08 -0400

    Remove OMF in favour of fisher
    - fisher: https://github.com/jorgebucaran/fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,12 @@
 set theme_complete_pathset yes
 
+# install fisher
+if not functions -q fisher
+  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  fish -c fisher
+end
+
 # ---Shopify Specific---
 # for dev
 if test -f /opt/dev/dev.fish
