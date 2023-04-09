13534ff40c2224f45955829fe99a6dab0de2959b
Nilay Shah <5805831+Nilay-Shah@users.noreply.github.com>
2023-04-09 10:32:25 -0400

    remove fisher + plugins
    attempt to re create the terminal prompts without a plugin manager
    using a standalone pkg: starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,59 +1,6 @@
 set -xg DISABLE_SPRING true
 set theme_complete_pathset yes
 
-
-set SPACEFISH_PACKAGE_SHOW false
-set SPACEFISH_NODE_SHOW false
-set SPACEFISH_JULIA_SHOW false
-set SPACEFISH_DOCKER_SHOW false
-set SPACEFISH_RUBY_SHOW false
-set SPACEFISH_HASKELL_SHOW false
-set SPACEFISH_AWS_SHOW false
-set SPACEFISH_VENV_SHOW false
-set SPACEFISH_CONDA_SHOW false
-set SPACEFISH_PYENV_SHOW false
-set SPACEFISH_GOLANG_SHOW false
-set SPACEFISH_PHP_SHOW false
-set SPACEFISH_RUST_SHOW false
-set SPACEFISH_DOTNET_SHOW false
-set SPACEFISH_BATTERY_SHOW false
-set SPACEFISH_VI_MODE_SHOW false
-set SPACEFISH_JOBS_SHOW false
-
-set SPACEFISH_EXIT_CODE_SHOW true
-
-
-# enable the "Tomorrow Night" color theme
-set fish_color_autosuggestion 969896
-set fish_color_cwd_root red
-set fish_color_host normal
-set fish_color_quote b9ca4a
-set fish_color_user brgreen
-set fish_color_cancel -r
-set fish_color_end c397d8
-set fish_color_match --background=brblue
-set fish_color_redirection 70c0b1
-set fish_color_valid_path --underline
-set fish_color_command c397d8
-set fish_color_error d54e53
-set fish_color_normal normal
-set fish_color_search_match 'bryellow' '--background=brblack'
-set fish_color_comment e7c547
-set fish_color_escape 00a6b2
-set fish_color_operator 00a6b2
-set fish_color_selection 'white' '--bold' '--background=brblack'
-set fish_color_cwd green
-set fish_color_history_current --bold
-set fish_color_param 7aa6da
-set fish_color_status red
-
-# install fisher
-if not functions -q fisher
-  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-  fish -c fisher
-end
-
 # chruby-fish
 #source /usr/local/share/chruby/chruby.fish
 #source /usr/local/share/chruby/auto.fish
@@ -69,6 +16,7 @@ end
 set -x GOPATH $HOME
 set -x PATH $PATH $GOPATH/bin
 
+# ---Shopify Specific---
 # kube
 if set -q KUBECONFIG[1]
   set -x KUBECONFIG "$KUBECONFIG:/Users/nilay/.kube/config:/Users/nilay/.kube/config.shopify.cloudplatform:/Users/nilay/.kube/config.shopify.production-registry"
