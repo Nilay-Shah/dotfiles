1512bce1e5169b9514f23a4556bdce78ceab0c88
Nilay Shah <5805831+Nilay-Shah@users.noreply.github.com>
2023-04-17 09:42:48 -0400

    add back fish shell colour theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,30 @@
 set -xg DISABLE_SPRING true
 set theme_complete_pathset yes
 
+# enable the "Tomorrow Night" color theme
+set fish_color_autosuggestion 969896
+set fish_color_cwd_root red
+set fish_color_host normal
+set fish_color_quote b9ca4a
+set fish_color_user brgreen
+set fish_color_cancel -r
+set fish_color_end c397d8
+set fish_color_match --background=brblue
+set fish_color_redirection 70c0b1
+set fish_color_valid_path --underline
+set fish_color_command c397d8
+set fish_color_error d54e53
+set fish_color_normal normal
+set fish_color_search_match 'bryellow' '--background=brblack'
+set fish_color_comment e7c547
+set fish_color_escape 00a6b2
+set fish_color_operator 00a6b2
+set fish_color_selection 'white' '--bold' '--background=brblack'
+set fish_color_cwd green
+set fish_color_history_current --bold
+set fish_color_param 7aa6da
+set fish_color_status red
+
 # after homebrew 3.0.0, Intel & M1 macs have diff install prefixes. fix this
 # by supporting both (for now) by adding the new M1 mac install prefix to $PATH.
 # more info: https://brew.sh/2021/02/05/homebrew-3.0.0/
